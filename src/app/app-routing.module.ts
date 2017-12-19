e3ac93974c9c42a5e7f9947f213f97ef2e0456f0
lemon <liuhaoyang1221@hotmail.com>
2017-12-19 23:40:05 +0800

    add dashboardcomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IndexComponent } from './index/index.component';
 import { TraceComponent } from './trace/trace.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/index', pathMatch: 'full' },
-  { path: 'index', component: IndexComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'trace', component: TraceComponent }
 ];
  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {NgZorroAntdModule} from 'ng-zorro-antd';
 import {AppComponent} from './app.component';
-import { IndexComponent } from './index/index.component';
 import { RouterModule }   from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { TraceComponent } from './trace/trace.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        IndexComponent,
-        TraceComponent
+        TraceComponent,
+        DashboardComponent
     ],
     imports: [
         BrowserModule,
