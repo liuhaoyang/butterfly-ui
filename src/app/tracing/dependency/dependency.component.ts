7e6f557766117c3071fddc4d47e5436ccef0b60c
lemon <liuhaoyang1221@hotmail.com>
2018-01-01 13:31:49 +0800

    add echarts

--- src/app/tracing/dependency/dependency.component.ts
+++ src/app/tracing/dependency/dependency.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import * as echarts from 'echarts'
 
 @Component({
   selector: 'app-dependency',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DependencyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-  }
+    let divElement = <HTMLDivElement>document.getElementById('main');
 
+    console.log(divElement.id);
+
+    var myChart = echarts.init(divElement);
+
+    // 指定图表的配置项和数据
+    var option = {
+        title: {
+            text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+            data:['销量']
+        },
+        xAxis: {
+            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        yAxis: {},
+        series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+  }
 }
--- src/app/tracing/tracing-routing.module.ts
+++ src/app/tracing/tracing-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { componentFactoryName } from '@angular/compiler';
 import { FindTracesComponent } from './find-traces/find-traces.component';
 import { TraceComponent } from './trace/trace.component';
+import { DependencyComponent } from './dependency/dependency.component';
 
 const routes: Routes = [
   {
     path: 'tracing',
     children: [
       { path: 'find-traces', component: FindTracesComponent },
-      { path: 'trace/:id', component: TraceComponent }
+      { path: 'trace/:id', component: TraceComponent },
+      { path: 'dependencies', component: DependencyComponent }
     ]
   }
 ];
