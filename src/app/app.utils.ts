df61811a3f728d5983e9b6a4f3c0dd683fb3e769
Lemon <liuhaoyang1221@hotmail.com>
2018-01-23 00:31:18 +0800

    fix

--- src/app/app.utils.ts
+++ src/app/app.utils.ts
@@ -1,6 +1,6 @@
 export const utils = {
     
     toDisplayDuration(duration: number): string {
-        return duration < 1000 ? duration + " μs" : duration / 1000.0 + " ms";
+        return duration < 1000 ? duration + ' μs' : (duration / 1000.0).toFixed(2) + ' ms';
     }
 };
